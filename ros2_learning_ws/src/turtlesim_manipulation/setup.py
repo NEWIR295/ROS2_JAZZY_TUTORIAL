49bbd27eb7cc57a8ddd8d2673f807fc3efb1176b
NEWIR529 <newir295@gmail.com>
2024-12-10 19:40:49 +0200

    adding new python package

---
+++ ros2_learning_ws/src/turtlesim_manipulation/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtlesim_manipulation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='NEWIR',
+    maintainer_email='mohamed.mohamed0123@eng-st.cu.edu.eg',
+    description='package responsible to manipulate with turtlesim',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
